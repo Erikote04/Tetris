3ed51d55d99f6b888c649c1dfee32b83a2b8f301
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-08 17:31:52 +0100

    Getting elements to add functionality to the game

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const scoreDisplay = document.querySelector("#score");
+    const startBtn = document.querySelector("#start-btn")
 
+    const board = document.querySelector(".board");
+    let cells = Array.from(document.querySelectorAll(".board div"));
+    const COLUMNS = 10;
 });
